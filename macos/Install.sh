6552816499af05495fdd6b4f03e667d1e24f8c95
DiegoDev <144474823+CodeDiego15@users.noreply.github.com>
2024-08-25 01:25:31 -0400

    Update Install.sh

--- macos/Install.sh
+++ macos/Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Definir variables
+
 BUILD_DIR="./bin"
 BIN_NAME="Latte"
 INSTALL_DIR="/usr/local/bin"
@@ -8,7 +8,7 @@ BREW_URL="https://brew.sh/"
 
 
 
-# Función para comprobar si Homebrew está instalado
+
 check_brew() {
   if command -v brew &> /dev/null; then
     echo "Homebrew ya está instalado."
@@ -19,35 +19,35 @@ check_brew() {
   fi
 }
 
-# Función para instalar Homebrew
+
 install_brew() {
-  # Instalar Homebrew
+  
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   
-  # Añadir Homebrew al PATH (para zsh)
+
   if [ -f /usr/local/bin/brew ]; then
     echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
     source ~/.zshrc
   fi
 }
 
-# Crear el directorio de compilación si no existe
+
 mkdir -p $BUILD_DIR
 
-# Comprobar e instalar Homebrew si es necesario
+
 if ! check_brew; then
   install_brew
 fi
 
-# Compilar el programa
+
 echo "Compiling the Go program..."
 go build -o $BUILD_DIR/$BIN_NAME
 
-# Instalar el binario
+
 echo "Installing the binary to $INSTALL_DIR..."
 sudo cp $BUILD_DIR/$BIN_NAME $INSTALL_DIR/
 
-# Verificar la instalación
+
 if [ -f "$INSTALL_DIR/$BIN_NAME" ]; then
     echo "Installation successful! You can now use '$BIN_NAME' from anywhere."
 else
