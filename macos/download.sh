caa6d8ccb3cba129d1c2cadfc7bb27c973f029e0
DiegoDev <144474823+CodeDiego15@users.noreply.github.com>
2024-08-25 01:24:53 -0400

    Update download.sh

--- macos/download.sh
+++ macos/download.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Variables
+
 REPO_URL="https://github.com/CodeDiego15/LattePkg"
 CLONE_DIR="LattePkg"
 
-# Clonar el repositorio de GitHub
+
 echo "Clonando el repositorio desde $REPO_URL..."
 git clone $REPO_URL
 
-# Comprobar si el repositorio se ha clonado con éxito
+
 if [ -d "$CLONE_DIR" ]; then
     echo "Repositorio clonado con éxito en $CLONE_DIR"
 else
@@ -16,15 +16,14 @@ else
     exit 1
 fi
 
-# Navegar a la carpeta del repositorio
+
 cd $CLONE_DIR
 
-# Comprobar si el script de instalación existe
 if [ -f "Install.sh" ]; then
-    # Hacer que el script de instalación sea ejecutable
+    
     chmod +x Install.sh
 
-    # Ejecutar el script de instalación
+    
     echo "Ejecutando el script de instalación..."
     ./Install.sh
 
