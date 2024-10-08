3a2ecdbe79f919234e14284d4c5b3d512db5afb3
DiegoDev <diegodev@MacBook-Pro-de-DiegoDev.local>
2024-10-08 12:31:44 -0400

    cmd

---
+++ CLI/cmd.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func Command() *cobra.Command {
+	var rootCmd = &cobra.Command{
+		Use:   "Fleet",
+		Short: "A package manager modern",
+	}
+	rootCmd.AddCommand(
+		&cobra.Command{
+			Use:   "install [package]",
+			Short: "Install a package",
+			Args:  cobra.ExactArgs(1),
+			Run: func(cmd *cobra.Command, args []string) {
+
+			},
+		},
+		&cobra.Command{
+			Use:   "uninstall [package]",
+			Short: "Uninstall a package",
+			Args:  cobra.ExactArgs(1),
+			Run: func(cmd *cobra.Command, args []string) {
+
+			},
+		},
+		&cobra.Command{
+			Use:   "upgrade [package]",
+			Short: "Upgrade a package",
+			Args:  cobra.ExactArgs(1),
+			Run: func(cmd *cobra.Command, args []string) {
+
+			},
+		},
+		&cobra.Command{
+			Use:   "version",
+			Short: "Show the version of LattePkg",
+			Run: func(cmd *cobra.Command, args []string) {
+
+			},
+		},
+		&cobra.Command{
+			Use:   "search [package]",
+			Short: "Search the package",
+			Run: func(cmd *cobra.Command, args []string) {
+
+			},
+		},
+	)
+
+	return rootCmd
+}
